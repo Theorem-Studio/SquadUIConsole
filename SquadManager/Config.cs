4c237843f3282d8fdc9775367737cd9cebdf0a27
Leader <3251263056@qq.com>
2023-07-12 19:40:04 +0800

    update

---
+++ SquadManager/Config.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquadManager
+{
+    internal class Config
+    {
+        public string Host="";
+        public int Port;
+        public string Password="";
+        const string path = "config.json";
+        public static Config GetConfig()
+        {
+            return JsonConvert.DeserializeObject<Config>(File.ReadAllText(path));
+        }
+    }
+}
